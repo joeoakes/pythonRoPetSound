28e0994fd8c75aea812cc97cbc7ab44e9978e00a
joeoakes <jxo19@psu.edu>
2025-03-03 19:55:56 -0500

    Commit

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 
 # Initialize pygame mixer
 pygame.mixer.init()
+pygame.mixer.music.set_volume(1.0)  # Max volume (range is 0.0 to 1.0)
 
 # Load sound file
 sound = pygame.mixer.Sound("cat_purr.wav")  # Replace with your file
